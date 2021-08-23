43a03ca2249e65a595f544ba72a9ff89dc6fc2ad
ayurn <ninaweayur@gmail.com>
2021-08-23 23:58:11 +0530

    [Ayur] Add : getting data and transferring it through pyspark socket and plotting live analysis of tweets.

---
+++ Twitter_sentiment_analysis_socket/twitter_connection.py
@@ -0,0 +1,76 @@
+'''
+@Author: Ayur Ninawe
+@Date: 2021-08-23
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-08-23
+@Title : Program to create socket and fetch twitter data.
+'''
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+import socket
+import json
+from os import environ as env
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+api_key = env['API_KEY']
+api_secret = env['API_SECRET_KEY']
+access_token = env['ACCESS_TOKEN']
+access_secret = env['ACCESS_TOKEN_SECRET']
+
+class TweetsListener(StreamListener):
+  # tweet object listens for the tweets
+  def __init__(self, csocket):
+    self.client_socket = csocket
+  def on_data(self, data):
+    try:  
+      msg = json.loads( data )
+      print("new message")
+      # if tweet is longer than 140 characters
+      if "extended_tweet" in msg:
+        # add at the end of each tweet "t_end" 
+        self.client_socket\
+            .send(str(msg['extended_tweet']['full_text']+"t_end")\
+            .encode('utf-8')) 
+        time.sleep(1)
+        print(msg['extended_tweet']['full_text'])
+      else:
+        # add at the end of each tweet "t_end" 
+        self.client_socket\
+            .send(str(msg['text']+"t_end")\
+            .encode('utf-8'))
+        print(msg['text'])
+      return True
+    except BaseException as e:
+        print("Error on_data: %s" % str(e))
+    return True
+  def on_error(self, status):
+    print(status)
+    return True
+
+def sendData(c_socket, keyword):
+  print('start sending data from Twitter to socket')
+  # authentication based on the credentials
+  auth = OAuthHandler(api_key, api_secret)
+  auth.set_access_token(access_token, access_secret)
+  # start sending data from the Streaming API 
+  twitter_stream = Stream(auth, TweetsListener(c_socket))
+  twitter_stream.filter(track = keyword, languages=["en"])
+
+if __name__ == "__main__":
+    # server (local machine) creates listening socket
+    s = socket.socket()
+    host = "127.0.0.1"    
+    port = 9998
+    s.bind((host, port))
+    print('socket is ready')
+    # server (local machine) listens for connections
+    s.listen(4)
+    print('socket is listening')
+    # return the socket and the address on the other side of the connection (client side)
+    c_socket, addr = s.accept()
+    print("Received request from: " + str(addr))
+    # select here the keyword for the tweet data
+    sendData(c_socket, keyword = ['Marvel'])
